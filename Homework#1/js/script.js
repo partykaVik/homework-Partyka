38ebbf2c33d1e009912b46496c1a47ae2582481a
Viktor <partykav2017@gmail.com>
2021-06-04 20:41:28 +0300

    Homework#1

--- Homework#1/js/script.js
+++ Homework#1/js/script.js
@@ -3,37 +3,38 @@ const smartphone = 123.965;
 const smartWatch = 90.2345;
 
 
-const maxNumber = Math.max(headphone, smartphone, smartWatch);
-console.log(maxNumber);
+const maxNumber = Math.max( headphone, smartphone, smartWatch );
+console.log( maxNumber );
 
-const minNumber = Math.min(headphone, smartphone, smartWatch);
-console.log(minNumber);
+const minNumber = Math.min( headphone, smartphone, smartWatch );
+console.log( minNumber );
 
-const sumNumber = (headphone + smartphone + smartWatch);
-console.log(sumNumber);
+const sumNumber = ( headphone + smartphone + smartWatch );
+console.log( sumNumber ) ;
 
-const integerSum = (Math.floor(headphone)+Math.floor(smartphone)+Math.floor(smartWatch));
-console.log(integerSum);
+const integerSum = ( Math.floor( headphone ) + Math.floor( smartphone ) + Math.floor( smartWatch ) );
+console.log( integerSum );
 
-const hundredsSum = ((Math.round(integerSum/100))*100);
-console.log(hundredsSum);
+const hundredsSum = ( ( Math.round( integerSum / 100 ) ) * 100 );
+console.log( hundredsSum );
 
-const boolenValue = (integerSum % 2 == 0);
-console.log(boolenValue);
+const isEvenNumber = ( integerSum % 2 == 0 );
+console.log( isEvenNumber );
 
-const differenceSum = (500-sumNumber);
-console.log(differenceSum);
+const bill = 500;
+const differenceSum = ( bill - sumNumber );
+console.log( differenceSum );
 
-const averageValueToHundredth = ((Math.round((sumNumber/3)*100))/100);
-console.log(averageValueToHundredth);
+const averageValueToHundredth = ( ( Math.round ( ( sumNumber / 3 ) * 100 ) ) / 100);
+console.log( averageValueToHundredth );
 
-const discount = (Math.round(Math.random()*100));
-const profit = ((sumNumber/2)-((discount/100)*sumNumber))
-console.log(profit);
+const discount = ( Math.round( Math.random() * 100 ) );
+const profit = ( ( sumNumber / 2 ) - ( ( discount / 100 ) * sumNumber ) )
+console.log( profit );
 
 document.querySelector('.devices_price').innerHTML = (
     `
-    <li>Найбільшеціна: ${maxNumber}</li>
+    <li>Найбільша ціна: ${maxNumber}</li>
     <li>Найменше ціна: ${minNumber}</li>
     <li>Вартість всіх товарів: ${sumNumber}</li>
     <li>Сума без копійок з округлення в меншу сторону: ${integerSum }</li>
