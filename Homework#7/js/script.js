49a85514cbf28208dedb6e72e153659c74fddbc0
Viktor <partykav2017@gmail.com>
2021-06-26 09:23:11 +0300

    Homework#7 added

---
+++ Homework#7/js/script.js
@@ -0,0 +1,58 @@
+const ukraine = {
+  tax: 0.195,
+  middleSalary: 1789,
+  vacancies: 11476
+};
+const latvia = {
+  tax: 0.25,
+  middleSalary: 1586,
+  vacancies: 3921
+};
+const litva = {
+  tax: 0.15,
+  middleSalary: 1509,
+  vacancies: 1114
+};
+
+
+//1. 
+function getMyTaxes(salary) { 
+  return salary*this.tax 
+  } 
+  console.log(`Як IT-спеціаліст з 5000$ в Україні ви сплатите ${getMyTaxes.call(ukraine, 5000)} $ податків`);
+   
+  //2. 
+  function getMiddleTaxes(country){ 
+    return this.middleSalary*this.tax 
+  } 
+   
+  console.log(`У середньому IT-спеціалісти в Україні сплачують ${getMiddleTaxes.call(ukraine)}$ податків`) 
+   
+  //3. 
+  function getTotalTaxes(country){ 
+    return this.middleSalary*this.tax*this.vacancies 
+  } 
+   
+  console.log(`Загалом IT-спеціалісти в Україні сплачують ${getTotalTaxes.call(ukraine)}$ податків`)
+
+//4
+function getMySalary(country) {
+  const infoItInCountry = {}
+  const salary = getRandomNumber(1500, 2000);
+  const taxes = salary*this.tax;
+  const profit = salary - taxes
+  Object.assign(infoItInCountry, {"salary": salary}, {"taxes" :taxes},{"profit": profit} )
+    return infoItInCountry
+}
+ 
+const timerId = setInterval(() => {
+  console.log( getMySalary.call(ukraine) );
+}, 10000);
+
+setTimeout(() => {
+  clearInterval(timerId);
+}, 30000);
+ 
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
