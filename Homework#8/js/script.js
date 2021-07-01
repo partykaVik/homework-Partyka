52c61e35d844ffe285800752de773f3c8782778f
Viktor <partykav2017@gmail.com>
2021-07-01 19:05:02 +0300

    :Homework#8 added

--- Homework#8/js/script.js
+++ Homework#8/js/script.js
@@ -1,58 +1,85 @@
-const ukraine = {
-  tax: 0.195,
-  middleSalary: 1789,
-  vacancies: 11476
-};
-const latvia = {
-  tax: 0.25,
-  middleSalary: 1586,
-  vacancies: 3921
-};
-const litva = {
-  tax: 0.15,
-  middleSalary: 1509,
-  vacancies: 1114
+// Basic
+class Student {
+  constructor(fullName, university, course, ...marks){
+    this.fullName = fullName,
+    this.university = university,
+    this.course = course,
+    this.marks = marks
+    this.dismiss = function() {
+      return this.marks = null;
+    }
+    this.recover = function() {
+      return this.marks = marks;
+    }
+     }
+ get studentInfo() {
+ return `Студент ${this.course}го курсу ${this.university} ${this.fullName}`
+ }
+ set addedStudentMark(mark){ 
+   if (this.marks === null) return null
+   return this.marks.push(mark)
+ }
+ get averageMarks() {
+  if (this.marks === null) 
+  return null
+  return this.marks.reduce((prev, item) =>  (prev + item)) / this.marks.length
+  }
+
 };
 
 
-//1. 
-function getMyTaxes(salary) { 
-  return salary*this.tax 
-  } 
-  console.log(`Як IT-спеціаліст з 5000$ в Україні ви сплатите ${getMyTaxes.call(ukraine, 5000)} $ податків`);
-   
-  //2. 
-  function getMiddleTaxes(country){ 
-    return this.middleSalary*this.tax 
-  } 
-   
-  console.log(`У середньому IT-спеціалісти в Україні сплачують ${getMiddleTaxes.call(ukraine)}$ податків`) 
-   
-  //3. 
-  function getTotalTaxes(country){ 
-    return this.middleSalary*this.tax*this.vacancies 
-  } 
-   
-  console.log(`Загалом IT-спеціалісти в Україні сплачують ${getTotalTaxes.call(ukraine)}$ податків`)
-
-//4
-function getMySalary(country) {
-  const infoItInCountry = {}
-  const salary = getRandomNumber(1500, 2000);
-  const taxes = salary*this.tax;
-  const profit = salary - taxes
-  Object.assign(infoItInCountry, {"salary": salary}, {"taxes" :taxes},{"profit": profit} )
-    return infoItInCountry
+//advance
+class BudgetStudent extends Student {
+  constructor(fullName, university, course, ...marks){
+    super(fullName, university, course, ...marks,)
+    setInterval(() => this.getScholarship(), 30000);
+    this.getScholarship = function(){
+      if (this.averageMarks >= 4){ 
+        console.log('Вітаю, Ви отримуєте стипендію 1600грн')
+      }
+      else {
+        console.log('Нажаль Ви не отримуєте стипендію')
+      }
+    }
 }
- 
-const timerId = setInterval(() => {
-  console.log( getMySalary.call(ukraine) );
-}, 10000);
-
-setTimeout(() => {
-  clearInterval(timerId);
-}, 30000);
- 
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
 }
+
+console.group(`Basic`)
+
+const ostap = new Student ('Остап Бендер','Вищої Школи Психотерапії м.Одеса',3, 5,4,4,5)
+
+console.group(`Оцінки зараз`)
+  console.log(`Оцінки студента: ` + ostap.marks)
+  console.log(`Середня оцінка студента = `+ ostap.averageMarks)
+console.groupEnd()
+
+ostap.addedStudentMark = 5 
+
+console.group(`Оцінки після отримання ще однієї відмітки`)
+  console.log(`Оцінки студента стали: ` + ostap.marks)
+  console.log(`Середня оцінка студента тепер = `+ ostap.averageMarks)
+console.groupEnd()
+
+console.log(ostap.dismiss())
+console.group(`Студента виключено`)
+  console.log(`Оцінки студента: ` + ostap.marks)
+console.groupEnd()
+
+ostap.recover()
+
+console.group(`Студента поновлено`)
+  console.log(`Оцінки студента: ` + ostap.marks)
+  console.log(`Середня оцінка студента = `+ ostap.averageMarks)
+  console.log(ostap);
+console.groupEnd()
+
+console.groupEnd()
+
+//advance
+console.group(`Advance`)
+const viktor = new BudgetStudent ('Віктор Партика','"НТУУ КПІ"', 5, 5,4)
+console.group(`Студент на бюджеті`)
+viktor.getScholarship()
+console.groupEnd()
+
+console.groupEnd()
