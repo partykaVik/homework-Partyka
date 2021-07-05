0a4ff42771b04407274d68b425cc2a0557f6734f
Viktor <partykav2017@gmail.com>
2021-07-05 22:16:35 +0300

    Homework#8 added

--- Homework#8/js/script.js
+++ Homework#8/js/script.js
@@ -5,48 +5,50 @@ class Student {
     this.university = university,
     this.course = course,
     this.marks = marks
-    this.dismiss = function() {
-      return this.marks = null;
-    }
-    this.recover = function() {
-      return this.marks = marks;
-    }
-     }
- 
-get studentMarks (){
-  return this.marks
-}     
-
- get studentInfo() {
- return `Студент ${this.course}го курсу ${this.university} ${this.fullName}`
- }
- set addedStudentMark(mark){ 
-   if (this.marks === null) return null
-   return this.marks.push(mark)
- }
- get averageMarks() {
-  if (this.marks === null) 
-  return null
-  return this.marks.reduce((prev, item) =>  (prev + item)) / this.marks.length
+    this.dismiss = false
   }
 
-};
+    recover() {
+      this.dismiss = false 
+    }
 
+    expelled() {
+      this.dismiss = true;
+    }
+
+    get studentMarks (){
+      if (this.dismiss) return null
+      return this.marks
+    }     
+
+    set studentMarks (mark){
+      if (this.dismiss) return null
+      return this.marks.push(mark)           
+    }
+
+    studentInfo() {
+    return `Студент ${this.course}го курсу ${this.university} ${this.fullName}`
+    }
+
+    averageMarks() {
+      if (this.dismiss) 
+      return null
+      return this.marks.reduce((prev, item) => (prev + item)) / this.marks.length  
+    }
+};
 
 //advance
 class BudgetStudent extends Student {
   constructor(fullName, university, course, ...marks){
     super(fullName, university, course, ...marks,)
-    setInterval(() => this.getScholarship(), 30000);
-    this.getScholarship = function(){
-      if (this.averageMarks >= 4){ 
-        console.log('Вітаю, Ви отримуєте стипендію 1600грн')
-      }
-      else {
-        console.log('Нажаль Ви не отримуєте стипендію')
-      }
+  }
+  
+    getScholarship = () =>{
+      if (this.averageMarks() >= 4) 
+      setInterval(() => {console.log('Вітаю, Ви отримуєте стипендію 1600грн') }, 30000)
+      else 
+      setInterval(() => {console.log('Нажаль Ви не отримуєте стипендію') }, 30000)
     }
-}
 }
 
 console.group(`Basic`)
@@ -55,17 +57,17 @@ const ostap = new Student ('Остап Бендер','Вищої Школи Пс
 
 console.group(`Оцінки зараз`)
   console.log(`Оцінки студента: ` + ostap.studentMarks)
-  console.log(`Середня оцінка студента = `+ ostap.averageMarks)
+  console.log(`Середня оцінка студента = `+ ostap.averageMarks())
 console.groupEnd()
 
-ostap.addedStudentMark = 5 
+ostap.studentMarks = 5 
 
 console.group(`Оцінки після отримання ще однієї відмітки`)
   console.log(`Оцінки студента стали: ` + ostap.studentMarks)
-  console.log(`Середня оцінка студента тепер = `+ ostap.averageMarks)
+  console.log(`Середня оцінка студента тепер = `+ ostap.averageMarks())
 console.groupEnd()
 
-console.log(ostap.dismiss())
+ostap.expelled()
 console.group(`Студента виключено`)
   console.log(`Оцінки студента: ` + ostap.studentMarks)
 console.groupEnd()
@@ -74,15 +76,17 @@ ostap.recover()
 
 console.group(`Студента поновлено`)
   console.log(`Оцінки студента: ` + ostap.studentMarks)
-  console.log(`Середня оцінка студента = `+ ostap.averageMarks)
+  console.log(`Середня оцінка студента = `+ ostap.averageMarks())
 console.groupEnd()
 
 console.groupEnd()
 
 //advance
 console.group(`Advance`)
-const viktor = new BudgetStudent ('Віктор Партика','"НТУУ КПІ"', 5, 5,4)
+const viktor = new BudgetStudent ('Віктор Партика','"НТУУ КПІ"', 5, 5,5,5)
+
 console.group(`Студент на бюджеті`)
+console.log(viktor.averageMarks())
 viktor.getScholarship()
 console.groupEnd()
 
